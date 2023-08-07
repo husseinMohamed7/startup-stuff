7bc4c9eb0cab767552c9dd24ee0ea8c8f6d70334
Hussein Mohamed <hussein7m7m@outlook.com>
2023-08-07 10:03:36 +0300

    Delete startup.sh

--- startup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-## Back and Night light
-redshift -l [lon]:[lat] -t 5700:3600 -g 0.8 -m randr -v &
-xbacklight -set 50
-
-## Wifi startup
-doas wpa_supplicant -B -i wlp61s0 -c /etc/wpa_supplicant/wpa_supplicant.conf
-doas dhcpcd wlp61s0
